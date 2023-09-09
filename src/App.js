84766228ae4af81a299801e0fd3000ac2c520b59
CrimsonCoder42 <nostro37@gmail.com>
2023-09-09 10:20:30 -0400

    1:44:49

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-
+import React, { useState } from 'react';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: 'Coco covered Almonds'
+    },
+    {
+      id: 2,
+      checked: true,
+      item: 'Peanut Butter'
+    },
+    {
+      id: 3,
+      checked: false,
+      item: 'Bananas'
+    }
+  ]);
 
-  const name = 'Devin'
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item);
+
+    setItems(listItems)
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id)
+    setItems(listItems)
+    localStorage.setItem('shoppinglist', JSON.stringify(listItems))
+  }
+
 
   return (
     <div className="App">
-      <Header />
-      <Content />
-      <Footer />
+      <Header title={"Grocery List"} />
+      <Content
+        items={items}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+         />
+      <Footer length={items.length}/>
     </div>
   );
 }
--- src/Content.js
+++ src/Content.js
@@ -1,50 +1,35 @@
-import { useState } from 'react'
 import { FaTrashAlt } from 'react-icons/fa'
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: 'Coco covered Almonds'
-    },
-    {
-      id: 2,
-      checked: true,
-      item: 'Peanut Butter'
-    },
-    {
-      id: 3,
-      checked: false,
-      item: 'Bananas'
-    }
-  ])
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => 
-      item.id === id ? { ...item, checked: !item.checked } : item);
-    
-    setItems(listItems)
-  }
+const Content = ({items, handleCheck, handleDelete }) => {
 
 
   return (
     <main>
-      <ul>
-        {items.map((item) => (
-          <li className="item" key={item.id}>
-            <input
-              type="checkbox"
-              onChange={() => handleCheck(item.id)}
-              checked={item.checked}
-            />
-            <label>{item.item}</label>
-            <FaTrashAlt
-              role="button"
-              tabIndex="0" />
-          </li>
-        ))}
-      </ul>
+      {items.length ? (
+        <ul>
+          {items.map((item) => (
+            <li className="item" key={item.id}>
+              <input
+                type="checkbox"
+                onChange={() => handleCheck(item.id)}
+                checked={item.checked}
+              />
+              <label
+                style={(item.checked) ? { textDecoration: 'line-through' } : null}
+                onDoubleClick={() => handleCheck(item.id)}
+              >{item.item}</label>
+              <FaTrashAlt
+                onClick={() => handleDelete(item.id)}
+                role="button"
+                tabIndex="0" />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="empty">No items in your list</p>
+      )
+      }
+
 
 
     </main>
